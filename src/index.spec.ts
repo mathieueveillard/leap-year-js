1afb02f71cab18e4a557aa980fed1eafa367cdfe
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2020-12-16 18:49:35 +0100

    A possible implementation

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,35 @@
-import { systemUnderTest } from ".";
+import { isLeapYear } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+describe("A leap year is divisible by 4", function () {
+  it("should state that 2003 is NOT a leap year", function () {
+    expect(isLeapYear(2003)).toEqual(false);
+  });
+
+  it("should state that 2004 is a leap year", function () {
+    expect(isLeapYear(2004)).toEqual(true);
+  });
+
+  it("should state that 2008 is a leap year", function () {
+    expect(isLeapYear(2008)).toEqual(true);
+  });
+});
+
+describe("...but is not otherwise divisible by 100", function () {
+  it("should state that 2100 is NOT a leap year", function () {
+    expect(isLeapYear(2100)).toEqual(false);
+  });
+
+  it("should state that 2200 is NOT a leap year", function () {
+    expect(isLeapYear(2200)).toEqual(false);
+  });
+});
+
+describe("...unless it is also divisible by 400", function () {
+  it("should state that 2000 is a leap year", function () {
+    expect(isLeapYear(2000)).toEqual(true);
+  });
+
+  it("should state that 2400 is a leap year", function () {
+    expect(isLeapYear(2400)).toEqual(true);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export function systemUnderTest(): boolean {
-  return true;
+export function isLeapYear(year: number): boolean {
+  return (isMultipleOf(4)(year) && !isMultipleOf(100)(year)) || isMultipleOf(400)(year);
+}
+
+function isMultipleOf(n: number) {
+  return function (year: number): boolean {
+    return year % n === 0;
+  };
 }
